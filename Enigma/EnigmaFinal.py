117e580d65aa374f590c166f6032b8f4dfaee4e8
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-11-20 23:52:54 +0100

    Create arrays, wiring and get ready for encryption

--- Enigma/EnigmaFinal.py
+++ Enigma/EnigmaFinal.py
@@ -12,18 +12,49 @@ def ShuffleArray(Array):
     random.shuffle(Array)
     return Array
 
-
-class Rotor:
+# Enigma
+class Enigma:
     def __init__(self):
+        self.Text = ""
+        self.Encrypted = []
         self.Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        self.Encryption = ShuffleArray(CopyArray(self.Alpha))
+        self.Counter = 0 # Current Letter
+        self.InverseCounter = 0 # Counter for decryption starting @ last letter from encryption
+        # Rotors
+        self.A = ShuffleArray(CopyArray(self.Alpha))
+        self.B = ShuffleArray(CopyArray(self.Alpha))
+        self.C = ShuffleArray(CopyArray(self.Alpha))
 
     def Start(self):
-        print("Wiring: ", self.Encryption)
+        print("Wiring: ", self.A)
+        print("Wiring: ", self.B)
+        print("Wiring: ", self.C)
+    
+    def TextInput(self, TXT):
+        self.Text = str(TXT)
 
-A = Rotor()
-B = Rotor()
-C = Rotor()
-A.Start()
-B.Start()
-C.Start()
+    def Encrypt(self):
+        for X in range(0, len(self.Text)):
+            match self.Text[X]:
+                case ",":
+                    continue
+                case "'":
+                    continue
+                case " ":
+                    continue
+
+                case _:
+                    self.Encryptor()
+
+    def Encryptor(self): # Bug: Doesn't work like this needs L to be translated to be number
+        L = self.Counter # Counter = Current Letter
+        # Rotor 1
+        L = self.A[L]
+        # Rotor 2
+        L = self.B[L]
+        # Rotor 3
+        L = self.C[L]
+
+Enigma = Enigma()
+Enigma.Start()
+Enigma.TextInput("Hello, I'm a stinky bunny")
