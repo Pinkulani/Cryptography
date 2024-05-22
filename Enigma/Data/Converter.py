257fbb2cfacdf422d1549bc08376662f911a7985
Emilia <97615383+Pinkulani@users.noreply.github.com>
2024-05-22 20:20:20 +0200

    Better dynamic converter

--- Enigma/Data/Converter.py
+++ Enigma/Data/Converter.py
@@ -1,19 +1,26 @@
 class Converter:
-    def __init__(self):
-        self.Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    def __init__(self, Alphabet):
+        self.Alphabet = Alphabet
+        self.Len = len(self.Alphabet)
 
-    def Letter(self, Number): # A -> 0; B -> 1
-        Text = []
-        for X in range(0, len(Number)):
-            Text.append(self.Alphabet[Number[X]])
-
-        return Text
+    def LetterToNumber(self, Letter): # A -> 0; B -> 1
+        for X in range(0, self.Len):
+            if Letter == self.Alphabet[X]:
+                return X # Int
+            
+    def NumberToLetter(self, Number): # 0 -> A; 1 -> B
+        return self.Alphabet[Number] # Char
     
-    def Number(self, Letter): # 0 -> A; 1 -> B
-        Text = []
-        for X in range(0, len(Letter)):
-            for Y in range(0, len(self.Alphabet)):
-                if Letter[X] == self.Alphabet[Y]:
-                    Text.append(Y)
-
-        return Text
+    def LettersToNumbers(self, Letters): # ABCDE -> 01234
+        Numbers = []
+        for X in range(0, len(Letters)):
+            Numbers.append(self.LetterToNumber(Letters[X]))
+        
+        return Numbers # Array : Int
+    
+    def NumbersToLetters(self, Numbers): # 01234 -> ABCDE
+        Letters = []
+        for X in range(0, len(Numbers)):
+            Letters.append(self.NumberToLetter(Numbers[X]))
+        
+        return Letters # Array : String
