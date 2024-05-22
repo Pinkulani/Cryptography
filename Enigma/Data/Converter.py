d30d40eb7e447377585969a4fc0597f94121b45b
Emilia <97615383+Pinkulani@users.noreply.github.com>
2024-05-22 14:39:40 +0200

    Adding comment

--- Enigma/Data/Converter.py
+++ Enigma/Data/Converter.py
@@ -2,14 +2,14 @@ class Converter:
     def __init__(self):
         self.Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    def Letter(self, Number):
+    def Letter(self, Number): # A -> 0; B -> 1
         Text = []
         for X in range(0, len(Number)):
             Text.append(self.Alphabet[Number[X]])
 
         return Text
     
-    def Number(self, Letter):
+    def Number(self, Letter): # 0 -> A; 1 -> B
         Text = []
         for X in range(0, len(Letter)):
             for Y in range(0, len(self.Alphabet)):
