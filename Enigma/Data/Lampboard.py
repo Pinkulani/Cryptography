1a10ed01e56f285d33e170d0b3674831e0bdb21c
Emilia <97615383+Pinkulani@users.noreply.github.com>
2024-05-22 14:38:41 +0200

    Adding comment

--- Enigma/Data/Lampboard.py
+++ Enigma/Data/Lampboard.py
@@ -5,7 +5,7 @@ class Lampboard:
     def Input(self, Text):
         self.Text = Text
 
-    def Output(self):
+    def Output(self): # Format: AAAAA AAAAA AAAAA
         for X in range(0, len(self.Text)):
             if (X % 5 == 0) and (X != 0):
                 print("", self.Text[X], end="")
