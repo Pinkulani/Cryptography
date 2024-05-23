2f5a326c2acc27d81f743339890e7294e1701315
Emilia <97615383+Pinkulani@users.noreply.github.com>
2024-05-23 03:14:36 +0200

    Adding Model selecter

--- Enigma/Enigma.py
+++ Enigma/Enigma.py
@@ -1,8 +1,34 @@
 from Modules.Converter import *
+from Data import *
 
 class Enigma:
     def __init__(self):
-        print("Hello")
+        self.Settings("I")
+        self.Debug = True
 
-if __name__ == "__name__":
+    def Settings(self, Model):
+        match Model:
+            case "I":
+                Static = 0
+                System = 0
+                Rotors = ["I", "II", "III"]
+                Reflector = "B"
+            case "M3":
+                Static = 0
+                System = 0
+                Rotors = ["VI", "VII", "VIII"]
+                Reflector = "B"
+            case "M4":
+                Static = 0
+                System = 0
+                Rotors = ["VI", "VII", "VIII"]
+                Extra = "Beta"
+                Reflector = "B-M4"
+
+        if Model == "M4":
+            self.Data = Data(Static, System, Rotors, Reflector, Extra)
+        else:
+            self.Data = Data(Static, System, Rotors, Reflector)
+
+if __name__ == "__main__":
     App = Enigma()
