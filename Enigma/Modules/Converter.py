8a6420b72074f08734e74ea3354b6fc09b983f03
Emilia <97615383+Pinkulani@users.noreply.github.com>
2024-05-23 19:52:08 +0200

    Add datatypes

--- Enigma/Modules/Converter.py
+++ Enigma/Modules/Converter.py
@@ -1,24 +1,24 @@
 class Converter:
-    def __init__(self, System):
+    def __init__(self, System: str):
         self.System = System
         self.Len = len(self.System)
 
-    def LetterToNumber(self, Letter): # A -> 0; B -> 1
+    def LetterToNumber(self, Letter: str): # A -> 0; B -> 1
         for X in range(0, self.Len):
             if Letter == self.System[X]:
                 return X # Int
             
-    def NumberToLetter(self, Number): # 0 -> A; 1 -> B
-        return self.System[Number] # Char
+    def NumberToLetter(self, Number: int): # 0 -> A; 1 -> B
+        return self.System[Number] # String
     
-    def LettersToNumbers(self, Letters): # ABCDE -> 01234
+    def LettersToNumbers(self, Letters: str): # ABCDE -> 01234
         Numbers = []
         for X in range(0, len(Letters)):
             Numbers.append(self.LetterToNumber(Letters[X]))
         
         return Numbers # Array : Int
     
-    def NumbersToLetters(self, Numbers): # 01234 -> ABCDE
+    def NumbersToLetters(self, Numbers: int): # 01234 -> ABCDE
         Letters = []
         for X in range(0, len(Numbers)):
             Letters.append(self.NumberToLetter(Numbers[X]))
