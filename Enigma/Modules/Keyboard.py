03de6a661368da84dea32bbe0e5def959911aca6
Emilia <97615383+Pinkulani@users.noreply.github.com>
2024-05-24 15:04:28 +0200

    More readable keyboard

--- Enigma/Modules/Keyboard.py
+++ Enigma/Modules/Keyboard.py
@@ -1,29 +1,32 @@
 class Keyboard:
     def __init__(self, System: str):
         self.System = System
-        self.Len = len(self.Sytem)
-        self.Text = None
 
-    def Input(self):
-        self.Text = str(input("Input: "))
+    def Run(self) -> str:
+        Text = self.Input()
+        Text = self.Filter(Text) # Only keeping Enigma-friendly characters
+        return Text
 
-    def Filter(self): # Only keeping Enigma-friendly characters
-        Copy = self.Text
-        LenCopy = len(Copy)
-        self.Text = []
+    def Input(self) -> str:
+        return str(input("Input: "))
 
-        match self.Len: # Filter by Latin / Swedish or Numerical
-            case LatinSwedish if self.Len > 20:
-                for TextPosition in range(0, LenCopy):
-                    for SystemPosition in range(0, self.Len):
-                        if Copy[TextPosition].upper() == self.System[SystemPosition]:
-                            self.Text.append(Copy[TextPosition].upper())
+    def Filter(self, Text: str) -> str:
+        LenSystem = len(self.System)
+        LenText = len(Text)
+        Filtered = []
 
-            case Numerical if self.Len < 10:
-                for TextPosition in range(0, LenCopy):
-                    for Number in range(0, self.Len):
-                        if Copy[TextPosition] == self.System[Number]: # Only string comparision possible
-                            self.Text.append(Copy[TextPosition])
+        # Filter by Latin / Swedish or Numerical
+        match LenSystem:
+            case LatinSwedish if LenSystem > 20:
+                for PositionText in range(LenText):
+                    for PositionSystem in range(LenSystem):
+                        if Text[PositionText].upper() == self.System[PositionSystem]:
+                            Filtered.append(Text[PositionText].upper())
 
-    def ReturnText(self):
-        return self.Text
+            case Numerical if LenSystem < 10:
+                for PositionText in range(0, LenText):
+                    for Number in range(0, LenSystem):
+                        if Text[PositionText] == self.System[Number]: # Only string comparision possible
+                            Filtered.append(Text[PositionText])
+
+        return Filtered
