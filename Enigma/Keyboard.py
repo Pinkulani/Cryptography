4ed34b0593f0ea62b52dcd52581e20e7f88f9925
Emilia <97615383+Pinkulani@users.noreply.github.com>
2024-05-26 14:50:59 +0200

    Adding new keyboard class

---
+++ Enigma/Keyboard.py
@@ -0,0 +1,32 @@
+class Keyboard:
+    def __init__(self, System: str):
+        self.System = System
+
+    def Run(self) -> str:
+        Text = self.Input()
+        Text = self.Filter(Text) # Only keeping Enigma-friendly characters
+        return Text
+
+    def Input(self) -> str:
+        return str(input("Input: "))
+
+    def Filter(self, Text: str) -> str:
+        LenSystem = len(self.System)
+        LenText = len(Text)
+        Filtered = []
+
+        # Filter by Latin / Swedish or Numerical
+        match LenSystem:
+            case LatinSwedish if LenSystem > 20:
+                for PositionText in range(LenText):
+                    for PositionSystem in range(LenSystem):
+                        if Text[PositionText].upper() == self.System[PositionSystem]:
+                            Filtered.append(Text[PositionText].upper())
+
+            case Numerical if LenSystem < 20:
+                for PositionText in range(LenText):
+                    for Number in range(LenSystem):
+                        if Text[PositionText] == self.System[Number]: # Only string comparision possible
+                            Filtered.append(Text[PositionText])
+
+        return Filtered
