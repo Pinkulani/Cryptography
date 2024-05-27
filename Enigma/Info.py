6314bd19e0e8f6305e415bc2458d0d0861ac0013
Emilia <97615383+Pinkulani@users.noreply.github.com>
2024-05-27 02:25:19 +0200

    Better info file for data

--- Enigma/Info.py
+++ Enigma/Info.py
@@ -1,195 +1,127 @@
-def COMPNTS(ID: str) -> bool:
-    match ID: # [Cogwheel, Plugboard, Extra Rotor, Extra Reflector, Turnable Reflector]
-        case "D":
-            return [False, False, False, False, False]
-        case "K":
-            return [False, False, False, False, False]
-        case "I":
-            return [False, True, False, True, False]
-        case "M3":
-            return [False, True, False, True, False]
-        case "M4":
-            return [False, True, True, True, False]
-        case "A28":
-            return [True, False, False, False, False]
-        case "G31":
-            return [True, True, False, False, False]
-        case "Z":
-            return [True, False, False, False, True]
-        case "A-133":
-            return [False, False, False, False, False]
-        case "T":
-            return [False, False, False, False, False]
-            
-def COMPNTNAMES(ID: str) -> str:
-    match ID: # [System, Entry wheel, Reflector, Rotor 1, Rotor 2, Rotor 3, Extra Rotor, Special Reflector]
-        case "D":
-            return ["Latin", "ETW-D", "UKW-D", ["I-D", "II-D", "III-D"], "None", "None"]
-        case "K":
-            return ["Latin", "ETW-D", "UKW-D", ["I-K", "II-K", "III-K"], "None", "None"]
-        case "I":
-            return ["Latin", "ETW", "UKW-B", ["I", "II", "III"], "None", "None"]
-        case "M3":
-            return ["Latin", "ETW", "None", ["I", "II", "III"], "None", "UKW-Special-D"]
-        case "M4":
-            return ["Latin", "ETW", "UKW-B-Thin", ["I", "II", "III"], "Beta", "None"]
-        case "A28":
-            return ["Latin", "ETW-D", "UKW-A28", ["I-A28", "II-A28", "III-A28"], "None", "None"]
-        case "G31":
-            return ["Latin", "ETW", "UKW-G31", ["I-G31", "II-G31", "III-G31"], "None", "None"]
-        case "Z":
-            return ["Numerical", "ETW-Z", "UKW-Z", ["I-Z", "II-Z", "III-Z"], "None", "None"]
-        case "A-133":
-            return ["Swedish", "ETW-A-133", "UKW-A-133", ["I-A-133", "II-A-133", "III-A-133"], "None", "None"]
-        case "T":
-            return ["Latin", "ETW-T", "UKW-T", ["I-T", "II-T", "III-T"], "None", "None"]
-            
-def WRNG(IDs: str) -> str:
-    # [System, Entry wheel, Reflector, Rotor 1, Rotor 2, Rotor 3, Extra Rotor]
-    WRNG = []
-    WRNG.append(SYSTM(IDs[0]))
-    WRNG.append(NTRY(IDs[1]))
-    WRNG.append(REFLCTR(IDs[2]))
-    RTRS = []
-    for Number in range(3):
-        RTRS.append(RTR(IDs[3][Number]))
-    WRNG.append(RTRS)
-    WRNG.append(RTR(IDs[4]))
-    WRNG.append(REFLCTR(IDs[5]))
-    return WRNG
+System = {
+    "Latin": ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+    "Swedish": ("ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ"),
+    "Numerical": ("1234567890")
+}
+
+Entry = {
+    "ETW": ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+    "ETW-D": ("QWERTZUIOASDFGHJKPYXCVBNML"),
+    "ETW-Z": ("1234567890"),
+    "ETW-A-133": ("ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ"),
+    "ETW-T": ("KZROUQHYAIGBLWVSTDXFPNMCJE")
+}
+
+Rotor = {
+    # Commercial Rotors
+    "I-D": ("LPGSZMHAEOQKVXRFYBUTNICJDW", "Z"),
+    "II-D": ("SLVGBTFXJQOHEWIRZYAMKPCNDU", "Z"),
+    "III-D": ("CJGDPSHKTURAWZXFMYNQOBVLIE", "Z"),
+    "I-K": ("LPGSZMHAEOQKVXRFYBUTNICJDW", "Y"),
+    "II-K": ("SLVGBTFXJQOHEWIRZYAMKPCNDU", "E"),
+    "III-K": ("CJGDPSHKTURAWZXFMYNQOBVLIE", "N"),
+    # Military Rotors
+    "I": ("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q"),
+    "II": ("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E"),
+    "III": ("BDFHJLCPRTXVZNYEIWGAKMUSQO", "V"),
+    "IV": ("ESOVPZJAYQUIRHXLNFTGKDCMWB", "J"),
+    "V": ("VZBRGITYUPSDNHLXAWMJQOFECK", "Z"),
+    "VI": ("JPGVOUMFYQBENHZRDKASXLICTW", "ZM"),
+    "VII": ("NZJHGRCXMYSWBOUFAIVLPEKQDT", "ZM"),
+    "VIII": ("FKQHTLXOCBJSPDZRAMEWNIUYGV", "ZM"),
+    # Cogwheel Rotors
+    "I-A28": ("LPGSZMHAEOQKVXRFYBUTNICJDW", "SUVWZABCEFGIKLOPQ"),
+    "II-A28": ("SLVGBTFXJQOHEWIRZYAMKPCNDU", "STVYZACDFGHKMNQ"),
+    "III-A28": ("CJGDPSHKTURAWZXFMYNQOBVLIE", "UWXAEFHKMNR"),
+    "I-G31": ("DMTWSILRUYQNKFEJCAZBPGXOHV", "SUVWZABCEFGIKLOPQ"),
+    "II-G31": ("HQZGPJTMOBLNCIFDYAWVEUSRKX", "STVYZACDFGHKMNQ"),
+    "III-G31": ("UQNTLSZFMREHDPXKIBVYGJCWOA", "UWXAEFHKMNR"),
+    "I-Z": ("6418270359", "9"),
+    "II-Z": ("5841097632", "9"),
+    "III-Z": ("3581620794", "9"),
+    # Special Rotors
+    "I-A-133": ("PSBGÖXQJDHOÄUCFRTEZVÅINLYMKA", "Ä"),
+    "II-A-133": ("CHNSYÖADMOTRZXBÄIGÅEKQUPFLVJ", "Ä"),
+    "III-A-133": ("ÅVQIAÄXRJBÖZSPCFYUNTHDOMEKGL", "Ä"),
+    "I-T": ("KPTYUELOCVGRFQDANJMBSWHZXI", "WZEKQ"),
+    "II-T": ("UPHZLWEQMTDJXCAKSOIGVBYFNR", "WZFLR"),
+    "III-T": ("QUDLYRFEKONVZAXWHMGPJBSICT", "WZEKQ"),
+    "IV-T": ("CIWTBKXNRESPFLYDAGVHQUOJZM", "WZFLR"),
+    "V-T": ("UAXGISNJBVERDYLFZWTPCKOHMQ", "YCFKR"),
+    "VI-T": ("XFUZGALVHCNYSEWQTDMRBKPIOJ", "XEIMQ"),
+    "VII-T": ("BJVFTXPLNAYOZIKWGDQERUCHSM", "YCFKR"),
+    "VII-T": ("YMTPNZHWKODAJXELUQVGCBISFR", "XEIMQ")
+}
+
+Reflector = {
+    "UKW-D": ("IMETCGFRAYSQBZXWLHKDVUPOJN"),
+    "UKW-A": ("EJMZALYXVBWFCRQUONTSPIKHGD"),
+    "UKW-B": ("YRUHQSLDPXNGOKMIEBFZCWVJAT"),
+    "UKW-C": ("FVPJIAOYEDRZXWGCTKUQSBNMHL"),
+    "UKW-B-Thin": ("ENKQAUYWJICOPBLMDXZVFTHRGS"),
+    "UKW-C-Thin": ("RDOBJNTKVEHMLFCWZAXGYIPSUQ"),
+    "UKW-A28": ("IMETCGFRAYSQBZXWLHKDVUPOJN"),
+    "UKW-G31": ("RULQMZJSYGOCETKWDAHNBXPVIF"),
+    "UKW-Z": ("5079183642", "9"),
+    "UKW-A-133": ("LDGBÄNCPSKJAVFZHXUIÅRMQÖOTEY"),
+    "UKW-T": ("GEKPBTAUMOCNILJDXZYFHWVQSR"),
+    "None": ("None")
+}
+
+Extra = {
+    "Beta": ("LEYJVCNIXWPBQMDRTAKZGFUHOS"),
+    "Gamma": ("FSOKANUERHMBTIYCWLQPZXVGJD"),
+    "UKW-Special-D": ("A0ZXWVUTSRQPON0MLKIHGFEDCB"),
+    "None": ("None")
+}
+
+# System, Entry wheel, Reflector, Rotor 1, Rotor 2, Rotor 3, Extra Rotor, Special Reflector
+Model = {
+    # Commercial Rotors
+    "D": ("Latin", "ETW-D", "UKW-D", ["I-D", "II-D", "III-D"], "None", "None"),
+    "K": ("Latin", "ETW-D", "UKW-D", ["I-K", "II-K", "III-K"], "None", "None"),
+    # Military Rotors
+    "I": ("Latin", "ETW", "UKW-B", ["I", "II", "III"], "None", "None"),
+    "M3": ("Latin", "ETW", "None", ["I", "II", "III"], "None", "UKW-Special-D"),
+    "M4": ("Latin", "ETW", "UKW-B-Thin", ["I", "II", "III"], "Beta", "None"),
+    # Cogwheel Rotors
+    "A28": ("Latin", "ETW-D", "UKW-A28", ["I-A28", "II-A28", "III-A28"], "None", "None"),
+    "G31": ("Latin", "ETW", "UKW-G31", ["I-G31", "II-G31", "III-G31"], "None", "None"),
+    "Z": ("Numerical", "ETW-Z", "UKW-Z", ["I-Z", "II-Z", "III-Z"], "None", "None"),
+    # Special Rotors
+    "A-133": ("Swedish", "ETW-A-133", "UKW-A-133", ["I-A-133", "II-A-133", "III-A-133"], "None", "None"),
+    "T": ("Latin", "ETW-T", "UKW-T", ["I-T", "II-T", "III-T"], "None", "None")
+}
+
+class Info:
+    def __init__(self, ID: str):
+        self.Names = Model[ID]
+        self.Wiring = (System[self.Names[0]], Entry[self.Names[1]], Reflector[self.Names[2]], [Rotor[self.Names[3][0]], Rotor[self.Names[3][0]], Rotor[self.Names[3][0]]], Extra[self.Names[4]], Extra[self.Names[5]])
+        self.Check(ID)
+    
+    def Check(self, ID: str) -> bool:
+        # Double Step, Turnable Reflector, Plugboard, Extra Rotor, Special Reflector
+        Cogwheel = False
+        Turnable = False
+        Plugboard = False
+        Extra = False
+        Special = False
         
-def SYSTM(ID: str) -> str:
-    match ID:
-        case "Latin":
-            return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        case "Swedish":
-            return "ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ"
-        case "Numerical":
-            return "1234567890"
-            
-def NTRY(ID: str) -> str:
-    match ID:
-        case "ETW":
-            return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        case "ETW-D":
-            return "QWERTZUIOASDFGHJKPYXCVBNML"
-        case "ETW-Z":
-            return "1234567890"
-        case "ETW-A-133":
-            return "ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ"
-        case "ETW-T":
-            return "KZROUQHYAIGBLWVSTDXFPNMCJE"
-            
-def REFLCTR(ID: str) -> str:
-    match ID:
-        case "UKW-D":
-            return "IMETCGFRAYSQBZXWLHKDVUPOJN"
-        case "UKW-A":
-            return "EJMZALYXVBWFCRQUONTSPIKHGD"
-        case "UKW-B":
-            return "YRUHQSLDPXNGOKMIEBFZCWVJAT"
-        case "UKW-C":
-            return "FVPJIAOYEDRZXWGCTKUQSBNMHL"
-        case "UKW-B-Thin":
-            return "ENKQAUYWJICOPBLMDXZVFTHRGS"
-        case "UKW-C-Thin":
-            return "RDOBJNTKVEHMLFCWZAXGYIPSUQ"
-        case "UKW-Special-D":
-            return "A0ZXWVUTSRQPON0MLKIHGFEDCB"
-        case "UKW-A28":
-            return "IMETCGFRAYSQBZXWLHKDVUPOJN"
-        case "UKW-G31":
-            return "RULQMZJSYGOCETKWDAHNBXPVIF"
-        case "UKW-Z":
-            return ["5079183642", "9"]
-        case "UKW-A-133":
-            return "LDGBÄNCPSKJAVFZHXUIÅRMQÖOTEY"
-        case "UKW-T":
-            return "GEKPBTAUMOCNILJDXZYFHWVQSR"
-        case _:
-            return "None"
-            
-def RTR(ID: str) -> str:
-    match ID:
-        # Commercial machines
-        case "I-D":
-            return ["LPGSZMHAEOQKVXRFYBUTNICJDW", "Z"]
-        case "II-D":
-            return ["SLVGBTFXJQOHEWIRZYAMKPCNDU", "Z"]
-        case "III-D":
-            return ["CJGDPSHKTURAWZXFMYNQOBVLIE", "Z"]
-        case "I-K":
-            return ["LPGSZMHAEOQKVXRFYBUTNICJDW", "Y"]
-        case "II-K":
-            return ["SLVGBTFXJQOHEWIRZYAMKPCNDU", "E"]
-        case "III-K":
-            return ["CJGDPSHKTURAWZXFMYNQOBVLIE", "N"]
+        if (ID == "A28") or (ID == "G31") or (ID == "Z"):
+            Cogwheel = True
         
-        # Military machines
-        case "I":
-            return ["EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q"]
-        case "II":
-            return ["AJDKSIRUXBLHWTMCQGZNPYFVOE", "E"]
-        case "III":
-            return ["BDFHJLCPRTXVZNYEIWGAKMUSQO", "V"]
-        case "IV":
-            return ["ESOVPZJAYQUIRHXLNFTGKDCMWB", "J"]
-        case "V":
-            return ["VZBRGITYUPSDNHLXAWMJQOFECK", "Z"]
-        case "VI":
-            return ["JPGVOUMFYQBENHZRDKASXLICTW", "ZM"]
-        case "VII":
-            return ["NZJHGRCXMYSWBOUFAIVLPEKQDT", "ZM"]
-        case "VIII":
-            return ["FKQHTLXOCBJSPDZRAMEWNIUYGV", "ZM"]
-        case "Beta":
-            return "LEYJVCNIXWPBQMDRTAKZGFUHOS"
-        case "Gamma":
-            return "FSOKANUERHMBTIYCWLQPZXVGJD"
-            
-        # Cogwheel machines
-        case "I-A28":
-            return ["LPGSZMHAEOQKVXRFYBUTNICJDW", "SUVWZABCEFGIKLOPQ"]
-        case "II-A28":
-            return ["SLVGBTFXJQOHEWIRZYAMKPCNDU", "STVYZACDFGHKMNQ"]
-        case "III-A28":
-            return ["CJGDPSHKTURAWZXFMYNQOBVLIE", "UWXAEFHKMNR"]
-        case "I-G31":
-            return ["DMTWSILRUYQNKFEJCAZBPGXOHV", "SUVWZABCEFGIKLOPQ"]
-        case "II-G31":
-            return ["HQZGPJTMOBLNCIFDYAWVEUSRKX", "STVYZACDFGHKMNQ"]
-        case "III-G31":
-            return ["UQNTLSZFMREHDPXKIBVYGJCWOA", "UWXAEFHKMNR"]
-        case "I-Z":
-            return ["6418270359", "9"]
-        case "II-Z":
-            return ["5841097632", "9"]
-        case "III-Z":
-            return ["3581620794", "9"]
+        if (ID == "I") or (ID == "M3") or (ID == "M4") or (ID == "G31"):
+            Plugboard = True
+
+        if (ID == "Z"):
+            Turnable = True
+
+        if (ID == "M4"):
+            if self.Wiring[4] != "None":
+                Extra = True
+
+        if (ID == "I") or (ID == "M3") or (ID == "M4"):
+            if self.Wiring[5] != "None":
+                Special = True
         
-        # Special machines
-        case "I-A-133":
-            return ["PSBGÖXQJDHOÄUCFRTEZVÅINLYMKA", "Ä"]
-        case "II-A-133":
-            return ["CHNSYÖADMOTRZXBÄIGÅEKQUPFLVJ", "Ä"]
-        case "III-A-133":
-            return ["ÅVQIAÄXRJBÖZSPCFYUNTHDOMEKGL", "Ä"]
-        case "I-T":
-            return ["KPTYUELOCVGRFQDANJMBSWHZXI", "WZEKQ"]
-        case "II-T":
-            return ["UPHZLWEQMTDJXCAKSOIGVBYFNR", "WZFLR"]
-        case "III-T":
-            return ["QUDLYRFEKONVZAXWHMGPJBSICT", "WZEKQ"]
-        case "IV-T":
-            return ["CIWTBKXNRESPFLYDAGVHQUOJZM", "WZFLR"]
-        case "V-T":
-            return ["UAXGISNJBVERDYLFZWTPCKOHMQ", "YCFKR"]
-        case "VI-T":
-            return ["XFUZGALVHCNYSEWQTDMRBKPIOJ", "XEIMQ"]
-        case "VII-T":
-            return ["BJVFTXPLNAYOZIKWGDQERUCHSM", "YCFKR"]
-        case "VII-T":
-            return ["YMTPNZHWKODAJXELUQVGCBISFR", "XEIMQ"]
-            
-        case _:
-            return "None"
+        self.Flags = (Cogwheel, Turnable, Plugboard, Extra, Special)
