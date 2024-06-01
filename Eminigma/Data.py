e765d48e213e8ddab8983c93fd9e95ee3a75c3b2
Emilia <97615383+Pinkulani@users.noreply.github.com>
2024-06-01 22:55:28 +0200

    Data for every Enigma

---
+++ Eminigma/Data.py
@@ -0,0 +1,159 @@
+Data = dict()
+# "Reflectors" : (("Wiring", ""), ) <- Komma after Tuple = Treat as Single Element
+Data["Enigma A-133"] = {
+    "Rotors" : (("PSBGÖXQJDHOÄUCFRTEZVÅINLYMKA", "Ä"), ("CHNSYÖADMOTRZXBÄIGÅEKQUPFLVJ", "Ä"), ("ÅVQIAÄXRJBÖZSPCFYUNTHDOMEKGL", "Ä")),
+    "Reflectors" : (("LDGBÄNCPSKJAVFZHXUIÅRMQÖOTEY", ""), ),
+    "Entry" : "ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ",
+    "System" : "ABCDEFGHIJKLMNOPQRSTUVXYZÅÄÖ",
+    "Cogwheel" : False,
+    "Plugboard" : False,
+    "Turnable" : False,
+    "Extra" : False,
+    "Special" : False
+}
+Data["Enigma D"] = {
+    "Rotors" : (("LPGSZMHAEOQKVXRFYBUTNICJDW", "Z"), ("SLVGBTFXJQOHEWIRZYAMKPCNDU", "Z"), ("CJGDPSHKTURAWZXFMYNQOBVLIE", "Z")),
+    "Reflectors" : (("IMETCGFRAYSQBZXWLHKDVUPOJN", ""), ),
+    "Entry" : "QWERTZUIOASDFGHJKPYXCVBNML",
+    "System" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "Cogwheel" : False,
+    "Plugboard" : False,
+    "Turnable" : False,
+    "Extra" : False,
+    "Special" : False
+}
+Data["Enigma I"] = {
+    "Rotors" : (("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q"), ("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E"), ("BDFHJLCPRTXVZNYEIWGAKMUSQO", "V"), ("ESOVPZJAYQUIRHXLNFTGKDCMWB", "J"), ("VZBRGITYUPSDNHLXAWMJQOFECK", "Z")),
+    "Reflectors" : (("EJMZALYXVBWFCRQUONTSPIKHGD", ""), ("YRUHQSLDPXNGOKMIEBFZCWVJAT", ""), ("FVPJIAOYEDRZXWGCTKUQSBNMHL", "")),
+    "Entry" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "System" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "Cogwheel" : False,
+    "Plugboard" : True,
+    "Turnable" : False,
+    "Extra" : False,
+    "Special" : True
+}
+Data["Enigma K"] = {
+    "Rotors" : (("LPGSZMHAEOQKVXRFYBUTNICJDW", "Y"), ("SLVGBTFXJQOHEWIRZYAMKPCNDU", "E"), ("CJGDPSHKTURAWZXFMYNQOBVLIE", "N")),
+    "Reflectors" : (("IMETCGFRAYSQBZXWLHKDVUPOJN", ""), ),
+    "Entry" : "QWERTZUIOASDFGHJKPYXCVBNML",
+    "System" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "Cogwheel" : False,
+    "Plugboard" : False,
+    "Turnable" : False,
+    "Extra" : False,
+    "Special" : False
+}
+Data["Enigma KD"] = {
+    "Rotors" : (("VEZIOJCXKYDUNTWAPLQGBHSFMR", "SUYAEHLNQ"), ("HGRBSJZETDLVPMQYCXAOKINFUW", "SUYAEHLNQ"), ("NWLHXGRBYOJSAZDVTPKFQMEUIC", "SUYAEHLNQ")),
+    "Reflectors" : (("", ""), ),
+    "Entry" : "QWERTZUIOASDFGHJKPYXCVBNML",
+    "System" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "Cogwheel" : False,
+    "Plugboard" : False,
+    "Turnable" : False,
+    "Extra" : False,
+    "Special" : True
+}
+Data["Enigma M3"] = {
+    "Rotors" : (("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q"), ("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E"), ("BDFHJLCPRTXVZNYEIWGAKMUSQO", "V"), ("ESOVPZJAYQUIRHXLNFTGKDCMWB", "J"), ("VZBRGITYUPSDNHLXAWMJQOFECK", "Z"), ("JPGVOUMFYQBENHZRDKASXLICTW", "ZM"), ("NZJHGRCXMYSWBOUFAIVLPEKQDT", "ZM"), ("FKQHTLXOCBJSPDZRAMEWNIUYGV", "ZM")),
+    "Reflectors" : (("YRUHQSLDPXNGOKMIEBFZCWVJAT", ""), ("FVPJIAOYEDRZXWGCTKUQSBNMHL", "")),
+    "Entry" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "System" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "Cogwheel" : False,
+    "Plugboard" : True,
+    "Turnable" : False,
+    "Extra" : False,
+    "Special" : True
+}
+Data["Enigma M4"] = {
+    "Rotors" : (("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q"), ("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E"), ("BDFHJLCPRTXVZNYEIWGAKMUSQO", "V"), ("ESOVPZJAYQUIRHXLNFTGKDCMWB", "J"), ("VZBRGITYUPSDNHLXAWMJQOFECK", "Z"), ("JPGVOUMFYQBENHZRDKASXLICTW", "ZM"), ("NZJHGRCXMYSWBOUFAIVLPEKQDT", "ZM"), ("FKQHTLXOCBJSPDZRAMEWNIUYGV", "ZM")),
+    "Reflectors" : (("ENKQAUYWJICOPBLMDXZVFTHRGS", ""), ("RDOBJNTKVEHMLFCWZAXGYIPSUQ", "")),
+    "Entry" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "System" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "Cogwheel" : False,
+    "Plugboard" : True,
+    "Turnable" : False,
+    "Extra" : True,
+    "Special" : True
+}
+Data["Enigma T"] = {
+    "Rotors" : (("KPTYUELOCVGRFQDANJMBSWHZXI", "WZEKQ"), ("UPHZLWEQMTDJXCAKSOIGVBYFNR", "WZFLR"), ("QUDLYRFEKONVZAXWHMGPJBSICT", "WZEKQ"), ("CIWTBKXNRESPFLYDAGVHQUOJZM", "WZFLR"), ("UAXGISNJBVERDYLFZWTPCKOHMQ", "YCFKR"), ("XFUZGALVHCNYSEWQTDMRBKPIOJ", "XEIMQ"), ("BJVFTXPLNAYOZIKWGDQERUCHSM", "YCFKR"), ("YMTPNZHWKODAJXELUQVGCBISFR", "XEIMQ")),
+    "Reflectors" : (("IMETCGFRAYSQBZXWLHKDVUPOJN", ""), ),
+    "Entry" : "KZROUQHYAIGBLWVSTDXFPNMCJE",
+    "System" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "Cogwheel" : False,
+    "Plugboard" : False,
+    "Turnable" : False,
+    "Extra" : False,
+    "Special" : False
+}
+Data["Enigma Z"] = {
+    "Rotors" : (("6418270359", "9"), ("5841097632", "9"), ("3581620794", "9")),
+    "Reflectors" : (("5079183642", "9"), ),
+    "Entry" : "1234567890",
+    "System" : "1234567890",
+    "Cogwheel" : True,
+    "Plugboard" : False,
+    "Turnable" : True,
+    "Extra" : False,
+    "Special" : False
+}
+Data["Norway Enigma"] = {
+    "Rotors" : (("WTOKASUYVRBXJHQCPZEFMDINLG", "Q"), ("GJLPUBSWEMCTQVHXAOFZDRKYNI", "E"), ("JWFMHNBPUSDYTIXVZGRQLAOEKC", "V"), ("FGZJMVXEPBWSHQTLIUDYKCNRAO", "J"), ("HEJXQOTZBVFDASCILWPGYNMURK", "Z")),
+    "Reflectors" : (("MOWJYPUXNDSRAIBFVLKZGQCHET", ""), ),
+    "Entry" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "System" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "Cogwheel" : False,
+    "Plugboard" : False,
+    "Turnable" : False,
+    "Extra" : False,
+    "Special" : False
+}
+Data["Rocket I"] = {
+    "Rotors" : (("EVLPKUDJHTGSZFRABWYICOXNMQ", "Y"), ("HXMQKGJTSCZFLBERNAWYIDOVPU", "E"), ("JHDBSKYPZNMVXURECLIGQOAWTF", "N")),
+    "Reflectors" : (("DNSAJQIPGEXRWBVHFLCZYOMKUT", ""), ),
+    "Entry" : "QWERTZUIOASDFGHJKPYXCVBNML",
+    "System" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "Cogwheel" : False,
+    "Plugboard" : False,
+    "Turnable" : False,
+    "Extra" : False,
+    "Special" : False
+}
+Data["Swiss K"] = {
+    "Rotors" : (("PEZUOHXSCVFMTBGLRINQJWAYDK", "Y"), ("ZOUESYDKFWPCIQXHMVBLGNJRAT", "E"), ("EHRVXGAOBQUSIMZFLYNWKTPDJC", "N")),
+    "Reflectors" : (("IMETCGFRAYSQBZXWLHKDVUPOJN", ""), ),
+    "Entry" : "QWERTZUIOASDFGHJKPYXCVBNML",
+    "System" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "Cogwheel" : False,
+    "Plugboard" : False,
+    "Turnable" : False,
+    "Extra" : False,
+    "Special" : False
+}
+Data["Zählwerk A28"] = {
+    "Rotors" : (("LPGSZMHAEOQKVXRFYBUTNICJDW", "SUVWZABCEFGIKLOPQ"), ("SLVGBTFXJQOHEWIRZYAMKPCNDU", "STVYZACDFGHKMNQ"), ("CJGDPSHKTURAWZXFMYNQOBVLIE", "UWXAEFHKMNR")),
+    "Reflectors" : (("IMETCGFRAYSQBZXWLHKDVUPOJN", ""), ),
+    "Entry" : "QWERTZUIOASDFGHJKPYXCVBNML",
+    "System" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "Cogwheel" : True,
+    "Plugboard" : False,
+    "Turnable" : False,
+    "Extra" : False,
+    "Special" : False
+}
+Data["Zählwerk G31"] = {
+    "Rotors" : (("DMTWSILRUYQNKFEJCAZBPGXOHV", "SUVWZABCEFGIKLOPQ"), ("HQZGPJTMOBLNCIFDYAWVEUSRKX", "STVYZACDFGHKMNQ"), ("UQNTLSZFMREHDPXKIBVYGJCWOA", "UWXAEFHKMNR")),
+    "Reflectors" : (("RULQMZJSYGOCETKWDAHNBXPVIF", ""), ),
+    "Entry" : "QWERTZUIOASDFGHJKPYXCVBNML",
+    "System" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "Cogwheel" : True,
+    "Plugboard" : True,
+    "Turnable" : False,
+    "Extra" : False,
+    "Special" : False
+}
+
+for Enigma in Data.keys(): # Add Name to dictionaries
+        Data[Enigma]["Name"] = Enigma
